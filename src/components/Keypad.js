9a3ff065ea17b2bed1909d2ed49504e7d3c38c6d
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-02-20 11:49:06 +0300

    modified the Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,17 +1,14 @@
-// Code Keypad Component Here
+function Keypad() {
+  const handleChange = (event) => {
+    console.log(`Entering password: ${event.target.value}`);
+  };
 
-function Keypad (){
-    const handleChange = () => {
-        console.log('Entering password...');
-      };
-    
-      return (
-        <div>
-          <label htmlFor="password-input">Password:</label>
-          <input type="password" id="password-input" onChange={handleChange} />
-        </div>
-      );
-    
+  return (
+    <div>
+      <label htmlFor="password-input">Password:</label>
+      <input type="password" id="password-input" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
